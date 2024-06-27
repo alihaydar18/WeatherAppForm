a2647d6f9d3b20f54937965cc41bde13ff5b5ef0
aliha <aliha@DESKTOP-2N06KQ9>
2024-06-27 17:32:11 +0300

    Add project files.

---
+++ WeatherAppForm/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace WeatherAppForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            pictureBox1 = new PictureBox();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 22.2F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 162);
+            label1.Location = new Point(39, 33);
+            label1.Name = "label1";
+            label1.Size = new Size(190, 50);
+            label1.TabIndex = 0;
+            label1.Text = "İZMİR, TR";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 162);
+            label2.Location = new Point(39, 97);
+            label2.Name = "label2";
+            label2.Size = new Size(118, 31);
+            label2.TabIndex = 1;
+            label2.Text = "İZMİR, TR";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 162);
+            label3.Location = new Point(39, 147);
+            label3.Name = "label3";
+            label3.Size = new Size(85, 31);
+            label3.TabIndex = 2;
+            label3.Text = "Bugün";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 36F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 162);
+            label4.Location = new Point(280, 216);
+            label4.Name = "label4";
+            label4.Size = new Size(191, 81);
+            label4.TabIndex = 3;
+            label4.Text = "00,00";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(39, 216);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(214, 119);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 162);
+            label5.Location = new Point(446, 202);
+            label5.Name = "label5";
+            label5.Size = new Size(25, 31);
+            label5.TabIndex = 5;
+            label5.Text = "c";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 162);
+            label6.Location = new Point(39, 356);
+            label6.Name = "label6";
+            label6.Size = new Size(91, 31);
+            label6.TabIndex = 6;
+            label6.Text = "Rüzgar";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 162);
+            label7.Location = new Point(136, 356);
+            label7.Name = "label7";
+            label7.Size = new Size(85, 31);
+            label7.TabIndex = 7;
+            label7.Text = "Bugün";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 162);
+            label8.Location = new Point(39, 408);
+            label8.Name = "label8";
+            label8.Size = new Size(65, 31);
+            label8.TabIndex = 8;
+            label8.Text = "Nem";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 162);
+            label9.Location = new Point(136, 408);
+            label9.Name = "label9";
+            label9.Size = new Size(85, 31);
+            label9.TabIndex = 9;
+            label9.Text = "Bugün";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 162);
+            label10.Location = new Point(289, 297);
+            label10.Name = "label10";
+            label10.Size = new Size(85, 31);
+            label10.TabIndex = 10;
+            label10.Text = "Bugün";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 162);
+            label11.Location = new Point(446, 297);
+            label11.Name = "label11";
+            label11.Size = new Size(85, 31);
+            label11.TabIndex = 11;
+            label11.Text = "Bugün";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Control;
+            ClientSize = new Size(739, 552);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(pictureBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private PictureBox pictureBox1;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+    }
+}
---
+++ WeatherAppForm/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System.Xml.Linq;
+
+namespace WeatherAppForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            label2.Text = DateTime.Now.ToShortDateString();
+
+            string api = "d9abf9d2c97bb3df506948bf19229197";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?lat=38&lon=27&appid=d9abf9d2c97bb3df506948bf19229197&units=metric&mode=xml&lang=tr";
+            XDocument weather = XDocument.Load(connection);
+            var warm = weather.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+            var windy = weather.Descendants("speed").ElementAt(0).Attribute("value").Value;
+            var humidity = weather.Descendants("humidity").ElementAt(0).Attribute("value").Value;
+            var hava = weather.Descendants("weather").ElementAt(0).Attribute("value").Value;
+            var feels_like = weather.Descendants("feels_like").ElementAt(0).Attribute("value").Value;
+            label4.Text = warm.ToString();
+            label7.Text = windy + " m/s";
+            label9.Text = humidity + " %";
+            label10.Text = hava;
+            label11.Text = feels_like + " Hissedilen Sıcaklık";
+
+        }
+
+
+    }
+}
